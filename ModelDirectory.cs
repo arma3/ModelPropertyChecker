fda045514c536bd8af7dc8495edf0ca11abbf072
Dedmen Miller <dedmen@dedmen.de>
2018-12-21 21:21:39 +0100

    Show progressbar when loading models

--- ModelDirectory.cs
+++ ModelDirectory.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace ModelPropertyChecker
 {
 
-    public class ModelDirectory
+    public class ModelDirectory: INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+
         public ObservableCollection<Model> models { get; set; } = new ObservableCollection<Model>();
+        public bool IsLoading { get; set; }
+        public bool IsNotLoading => !IsLoading;
+        public int totalNumberOfModels { get; set; }
+        public bool isIndeterminate => totalNumberOfModels == 0;
+
 
         public async void LoadFromDirectory(string path, TaskScheduler uiThread)
         {
 
-            await Task.Run(() => { }).ContinueWith(t =>
-            {
-                models.Clear();
+            await Task.Run(() => { }).ContinueWith(t => { models.Clear(); }, uiThread);
 
-            }, uiThread);
-
-            
+            IsLoading = true;
+            OnPropertyChanged("IsLoading");
+            OnPropertyChanged("IsNotLoading");
+            totalNumberOfModels = 0;
+            OnPropertyChanged("totalNumberOfModels");
+            OnPropertyChanged("isIndeterminate");
 
             var tasks = ModelLoader.loadFromDirectory(path, true);
+            totalNumberOfModels = tasks.Count;
+            OnPropertyChanged("totalNumberOfModels");
+            OnPropertyChanged("isIndeterminate");
 
             while (tasks.Count > 0)
             {
@@ -27,7 +45,7 @@ namespace ModelPropertyChecker
 
                 tasks.Remove(finishedTask);
 
-#pragma warning disable 4014 //Don't want await here resharper :U
+    #pragma warning disable 4014 //Don't want await here resharper :U
                 finishedTask.ContinueWith(t =>
                 {
                     var model = t.Result;
@@ -36,9 +54,14 @@ namespace ModelPropertyChecker
                 }).ContinueWith(t =>
                 {
                     models.Add(t.Result);
+                    OnPropertyChanged("models");
                 }, uiThread);
-#pragma warning restore 4014
+    #pragma warning restore 4014
             }
+
+            IsLoading = false;
+            OnPropertyChanged("IsLoading");
+            OnPropertyChanged("IsNotLoading");
         }
 
 
